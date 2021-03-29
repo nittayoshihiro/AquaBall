803edf416cbc29ddc0ea3bc8e3076d334380cd88
nitta <yosshi3800@gmail.com>
2021-03-29 20:38:55 +0900

    playerの制作 ・スクリプトを作成（PlayerController）     基本操作の作成 mapの制作 ・スクリプトを制作（ DrillingMethod） 　途中

---
+++ Assets/Script/DrillingMethod.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// マップ自動生成、穴掘り法
+/// </summary>
+public class DrillingMethod : MonoBehaviour
+{
+    /// <summary>マップのサイズ（奇数）</summary>
+    [SerializeField] int m_size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //DrillingMethod();
+    }
+
+    /// <summary>
+    /// 穴掘り関数
+    /// </summary>
+    /// <param name="mapsize">マップサイズ</param>
+    /// <returns></returns>
+    int[,] Drilling(int mapsize)
+    {
+        int[,] mapdata = new int[mapsize, mapsize];
+        //奇数でないと成り立たないため
+        if (m_size % 2 == 1)
+        {
+            return mapdata;
+        }
+        else
+        {
+            Debug.Log("奇数ではないので作成不可");
+            return null;
+        }
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,8 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// プレイヤーの操作を管理する
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>重力</summary>
+    const float m_gravity = 9.81f;
+    /// <summary>重力をかける方向</summary>
+    Vector3 m_vector3 = new Vector3();
+    /// <summary>重力規模</summary>
+    [SerializeField] float m_gravityScale = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //unityエディター上で動かしている場合
+        if (Application.isEditor)
+        {
+            //キー入力を検知ベクトルを設定
+            m_vector3.x = Input.GetAxis("Horizontal");
+            m_vector3.z = Input.GetAxis("Vertical");
+
+        }
+        //スマホデバッグ用
+        else
+        {
+            //加速度センサーの入力をUnity空間の軸にマッピングする(座標軸が異なるため)
+            m_vector3.x = Input.acceleration.x;
+            m_vector3.z = Input.acceleration.y;
+            m_vector3.y = Input.acceleration.z;
+        }
+        //シーンの重力を入力ベクトルの方向に合わせて変化させる
+        Physics.gravity = m_gravity * m_vector3.normalized * m_gravityScale;
+        Debug.Log(Physics.gravity);
     }
 }
