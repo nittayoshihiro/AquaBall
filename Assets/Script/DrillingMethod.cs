866e34b90ee3d72da4885faa97eedc74118a1221
nitta <yosshi3800@gmail.com>
2021-04-13 22:13:52 +0900

    DrillingMethod.csのスタックオーバーを解決すべく作業を行なった本日分をあげる

--- Assets/Script/DrillingMethod.cs
+++ Assets/Script/DrillingMethod.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+
 /// <summary>
 /// マップ自動生成、穴掘り法
 /// </summary>
@@ -11,26 +12,36 @@ public class DrillingMethod : MonoBehaviour
     /// <summary>マップの名前</summary>
     [SerializeField] string m_mapName = "map";
     /// <summary>マップのサイズ（奇数）</summary>
+    [Header("マップサイズ※５以上の奇数を入力")]
     [SerializeField] int m_mapSize;
     /// <summary>作成する階層</summary>
     [SerializeField] int m_mapFloor;
     /// <summary>キューブプレハブ</summary>
-    [SerializeField] GameObject m_cubePrefab;
+    [SerializeField] GameObject m_cubePrefab = null;
     /// <summary>スタートキューブプレハブ</summary>
-    [SerializeField] GameObject m_startCubePrefab;
+    [SerializeField] GameObject m_startCubePrefab = null;
     /// <summary>中間地点キューブプレハブ</summary>
-    [SerializeField] GameObject m_middleCubePrefab;
+    [SerializeField] GameObject m_middleCubePrefab = null;
     /// <summary>ゴールキューブプレハブ</summary>
-    [SerializeField] GameObject m_goalCubePrefab;
+    [SerializeField] GameObject m_goalCubePrefab = null;
     /// <summary>プレイヤープレハブ</summary>
-    [SerializeField] GameObject m_playerPrefab;
+    [SerializeField] GameObject m_playerPrefab = null;
     /// <summary>デバッグonoff</summary>
     [SerializeField] bool m_debug = true;
+    /// <summary>マップデータ</summary>
+    private MapState[,] m_mapdata;
+    /// <summary>ゴールを作成するかの判定</summary>
+    private bool m_goalpoint;
+    private List<Vector3Int> m_startPos = null;
     //[SerializeField] int m_startx, m_startz;
     int m_goalx, m_goalz;
 
+    /// <summary>
+    /// マッピングテスト用
+    /// </summary>
     public void TestMapping()
     {
+        m_startPos = new List<Vector3Int>();
         //子オブジェクトを削除（マップ）
         foreach (Transform child in gameObject.transform)
         {
@@ -38,207 +49,125 @@ public class DrillingMethod : MonoBehaviour
         }
         GameObject player = GameObject.Find("Player(Clone)");
         Destroy(player);
-        CreateMap(m_mapName, m_mapFloor);
+        ResetMapData();
+        Debug.Log(m_mapdata.GetLength(0) + " " + m_mapdata.GetLength(1));
+        m_mapdata[1, 1] = MapState.Road;
+        DigHole(new Vector3Int(1, 0, 1));
+        CreateFloorMap(m_mapdata, m_mapdata, m_mapName, m_mapFloor);
     }
 
     // Start is called before the first frame update
     public void MappingStart()
     {
-        CreateMap(m_mapName, m_mapFloor);
+
     }
 
     /// <summary>
-    /// マップを作成
+    /// マップデータを初期化（全て壁のデータにする）
     /// </summary>
-    ///<param name="mapname">マップの名前</param>
-    ///<param name="mapfloor">作る階層</param>
-    public void CreateMap(string mapname, int mapfloor)
+    private void ResetMapData()
     {
-        CreateFloorMap(Drilling(m_mapSize, 1, 1), SetUpMapData(m_mapSize), mapname, mapfloor);
-    }
-
-    /// <summary>
-    /// 穴掘り関数 (0：道　１：壁)
-    /// </summary>
-    /// <param name="mapsize">マップサイズ</param>
-    /// <param name="startx">スタートポジションx</param>
-    /// <param name="starty">スタートポジションy</param>
-    MapState[,] Drilling(int mapsize, int startx, int starty)
-    {
-        //奇数でないと成り立たないため
-        if (m_mapSize % 2 == 1)
+        m_mapdata = new MapState[m_mapSize, m_mapSize];
+        for (int x = 0; x < m_mapSize; x++)
         {
-            //mapデータを作成　初期化（全て壁）
-            MapState[,] mapdata = SetUpMapData(mapsize);
-            //穴掘り
-            mapdata[startx, starty] = MapState.Start;//スタート
-            DigHole(mapdata, startx, starty);
-            //ReversDigHole(mapdata, m_goalx, m_goalz);
-            return mapdata;
-        }
-        else
-        {
-            Debug.LogError("奇数ではないので作成不可");
-            return null;
-        }
-    }
-
-    /// <summary>
-    /// マップの初期化
-    /// </summary>
-    /// <param name="mapsize"></param>
-    /// <returns></returns>
-    private MapState[,] SetUpMapData(int mapsize)
-    {
-        MapState[,] mapdata = new MapState[mapsize, mapsize];
-        for (int x = 0; x < mapsize; x++)
-        {
-            for (int z = 0; z < mapsize; z++)
+            for (int z = 0; z < m_mapSize; z++)
             {
-                mapdata[x, z] = MapState.Wall;
-            }
-        }
-        return mapdata;
-    }
-
-
-    /// <summary>
-    /// スタートからゴールまで穴を掘る
-    /// </summary>
-    /// <param name="mapdata">現在のマップデータ</param>
-    /// <param name="nowposx">掘り進めている所X</param>
-    /// <param name="nowposz">掘り進めている所Y</param>
-    private void DigHole(MapState[,] mapdata, int nowposx, int nowposz)
-    {
-        //上下左右で進める数字を保存する（掘られている所）
-        int[] ramDirection = new[] { 0 };
-        if (nowposz + 2 < m_mapSize)
-        {
-            if (mapdata[nowposx, nowposz + 2] == MapState.Wall)//上
-            {
-                System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
-                ramDirection[ramDirection.Length - 1] = 1;
-            }
-        }
-        if (0 < nowposz - 2)
-        {
-            if (mapdata[nowposx, nowposz - 2] == MapState.Wall)//下
-            {
-                System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
-                ramDirection[ramDirection.Length - 1] = 2;
-            }
-        }
-        if (0 < nowposx - 2)
-        {
-            if (mapdata[nowposx - 2, nowposz] == MapState.Wall)//左
-            {
-                System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
-                ramDirection[ramDirection.Length - 1] = 3;
-            }
-        }
-        if (nowposx + 2 < m_mapSize)
-        {
-            if (mapdata[nowposx + 2, nowposz] == MapState.Wall)//右
-            {
-                System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
-                ramDirection[ramDirection.Length - 1] = 4;
-            }
-        }
-
-        //掘り進められなかったらゴールにする
-        if (ramDirection.Length == 1)
-        {
-            mapdata[nowposx, nowposz] = MapState.Goal;
-        }
-        else
-        {
-            //デバッグでするため変数に入れる
-            int ramTestDirection = ramDirection[Random.Range(1, ramDirection.Length)];
-            if (m_debug)
-            {
-                Debug.Log(ramTestDirection);
-            }
-            //ランダムで進行方向を決める
-            switch (ramTestDirection)
-            {
-                case 1:
-                    mapdata[nowposx, nowposz + 1] = MapState.Road;
-                    mapdata[nowposx, nowposz + 2] = MapState.Road;
-                    DigHole(mapdata, nowposx, nowposz + 2);
-                    break;
-
-                case 2:
-                    mapdata[nowposx, nowposz - 1] = MapState.Road;
-                    mapdata[nowposx, nowposz - 2] = MapState.Road;
-                    DigHole(mapdata, nowposx, nowposz - 2);
-                    break;
-
-                case 3:
-                    mapdata[nowposx - 1, nowposz] = MapState.Road;
-                    mapdata[nowposx - 2, nowposz] = MapState.Road;
-                    DigHole(mapdata, nowposx - 2, nowposz);
-                    break;
-
-                case 4:
-                    mapdata[nowposx + 1, nowposz] = MapState.Road;
-                    mapdata[nowposx + 2, nowposz] = MapState.Road;
-                    DigHole(mapdata, nowposx + 2, nowposz);
-                    break;
+                m_mapdata[x, z] = MapState.Wall;
             }
         }
     }
 
     /// <summary>
-    /// ゴールから掘り残しを掘る
+    /// 穴を掘る
     /// </summary>
-    /// <param name="mapdata">マップデータ</param>
-    /// <param name="nowposx">戻り所X</param>
-    /// <param name="nowposz">戻り所Y</param>
-    private void ReversDigHole(MapState[,] mapdata, int nowposx, int nowposz)
+    /// <param name="mapPos"></param>
+    public void DigHole(Vector3Int mapPos)
     {
-        if (mapdata[nowposx, nowposz] == MapState.Start)
+        while (true)
         {
-            Debug.Log("終了");
-        }
-        else
-        {
-            //上下左右で戻るますを探す
-            if (nowposz + 2 < m_mapSize)
+            //上下左右で進める数字を保存する（掘れる所 上１ 下２ 左３ 右４）
+            int[] ramDirection = new int[] { 0 };
+            if (mapPos.z + 2 < m_mapSize)
             {
-                if (mapdata[nowposx, nowposz + 1] == MapState.Road)//上
+                Debug.Log(mapPos.z + 2);
+                if (m_mapdata[mapPos.x, mapPos.z + 2] == MapState.Wall)
                 {
-                    ReversDigHole(mapdata, nowposx, nowposz + 2);
-                    DigHole(mapdata, nowposx, nowposz + 2);
+                    System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
+                    ramDirection[ramDirection.Length - 1] = 1;
                 }
             }
-            if (0 < nowposz - 2)
+            if (0 < mapPos.z - 2)
             {
-                if (mapdata[nowposx, nowposz - 1] == MapState.Road)//下
+                Debug.Log(mapPos.z - 2);
+                if (m_mapdata[mapPos.x, mapPos.z - 2] == MapState.Wall)
                 {
-                    ReversDigHole(mapdata, nowposx, nowposz - 2);
-                    DigHole(mapdata, nowposx, nowposz - 2);
+                    System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
+                    ramDirection[ramDirection.Length - 1] = 2;
                 }
             }
-            if (0 < nowposx - 2)
+            if (0 < mapPos.x - 2)
             {
-                if (mapdata[nowposx - 1, nowposz] == MapState.Road)//左
+                Debug.Log(mapPos.x - 2);
+                if (m_mapdata[mapPos.x - 2, mapPos.z] == MapState.Wall)
                 {
-                    ReversDigHole(mapdata, nowposx - 2, nowposz);
-                    DigHole(mapdata, nowposx - 2, nowposz);
+                    System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
+                    ramDirection[ramDirection.Length - 1] = 3;
                 }
             }
-            if (nowposx + 2 < m_mapSize)
+            if (mapPos.x + 2 < m_mapSize)
             {
-                if (mapdata[nowposx + 1, nowposz] == MapState.Road)//右
+                Debug.Log(mapPos.x + 2);
+                if (m_mapdata[mapPos.x + 2, mapPos.z] == MapState.Wall)
                 {
-                    ReversDigHole(mapdata, nowposx + 2, nowposz);
-                    DigHole(mapdata, nowposx + 2, nowposz);
+                    System.Array.Resize(ref ramDirection, ramDirection.Length + 1);
+                    ramDirection[ramDirection.Length - 1] = 4;
+                }
+            }
+            //掘り進められなかったら
+            if (ramDirection.Length == 1)
+            {
+                ReStartDigHole();
+                break;
+            }
+            else
+            {
+                switch (ramDirection[Random.Range(1, ramDirection.Length)])
+                {
+                    case 1:
+                        m_mapdata[mapPos.x, ++mapPos.z] = MapState.Road;
+                        m_mapdata[mapPos.x, ++mapPos.z] = MapState.Road;
+                        m_startPos.Add(new Vector3Int(mapPos.x, 0, mapPos.z + 2));
+                        break;
+                    case 2:
+                        m_mapdata[mapPos.x, --mapPos.z] = MapState.Road;
+                        m_mapdata[mapPos.x, --mapPos.z] = MapState.Road;
+                        m_startPos.Add(new Vector3Int(mapPos.x, 0, mapPos.z - 2));
+                        break;
+                    case 3:
+                        m_mapdata[--mapPos.x, mapPos.z] = MapState.Road;
+                        m_mapdata[--mapPos.x, mapPos.z] = MapState.Road;
+                        m_startPos.Add(new Vector3Int(mapPos.x - 2, 0, mapPos.z));
+                        break;
+                    case 4:
+                        m_mapdata[++mapPos.x, mapPos.z] = MapState.Road;
+                        m_mapdata[++mapPos.x, mapPos.z] = MapState.Road;
+                        m_startPos.Add(new Vector3Int(mapPos.x + 2, 0, mapPos.z));
+                        break;
                 }
             }
         }
-
     }
 
+    /// <summary>
+    /// 掘れる可能性のあるポジションからランダムで堀る関数
+    /// </summary>
+    private void ReStartDigHole()
+    {
+        int ramIndex = Random.Range(0, m_startPos.Count);
+        Vector3Int startPos = m_startPos[ramIndex];
+        m_startPos.RemoveAt(ramIndex);
+        DigHole(startPos);
+    }
 
     /// <summary>
     ///　マップのデータを元にフロアマップを作成
@@ -246,7 +175,7 @@ public class DrillingMethod : MonoBehaviour
     /// <param name="mapdata">マップデータ</param>
     /// <param name="mapname">親オブジェクトになる名前d</param>
     /// <param name="maxfloor">作る高さ(maxの高さ)</param>
-    void CreateFloorMap(MapState[,] mapdata, MapState[,] floormapdata, string mapname, float maxfloor)
+    private void CreateFloorMap(MapState[,] mapdata, MapState[,] floormapdata, string mapname, float maxfloor)
     {
         //親になるオブジェクトを生成
         GameObject mapObject = new GameObject(mapname);
