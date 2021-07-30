d08bbf6c8fe5af1ab010166f466e38f06b2ec360
nitta <yosshi3800@gmail.com>
2021-07-30 11:06:05 +0900

    10進数を32進数に変換する機能を作成中20210730

---
+++ Assets/Script/BaseConversion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class BaseConversion
+{
+    private static string[] num32 = new[]
+     {"1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G",
+        "H","I","J","K","L","N","M","O","P","Q","R","S","T","U","V"};
+
+    /// <summary>
+    /// 32までの数字を32進数で返します
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public static string Conversion32(int num)
+    {
+        string conversionnum;
+        conversionnum = num32[num - 1];
+        return conversionnum;
+    }
+
+    /// <summary>
+    /// 10進数を32進数で返します。
+    /// </summary>
+    /// <param name="num"></param>
+    public void FullConvers32(int num)
+    {
+        string conversionnum;
+        int onesPlace;
+        onesPlace = num % 32;
+        int tensPlace;
+        tensPlace = num / 32;
+        
+        
+    }
+}
