10a550bc5633eaa39fa3b86630ac8eb200494627
nitta <yosshi3800@gmail.com>
2021-09-09 10:18:13 +0900

    ランダムをシード値を使って管理するテストシーンを作りました。

---
+++ Assets/Script/RandomSeed.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSeed
+{
+    /// <summary></summary>
+    private Random.State m_state;
+
+    /// <summary>
+    ///　インスタンス時にrandomのシード値を設定する
+    /// </summary>
+    /// <param name="seed"></param>
+    public RandomSeed(int seed)
+    {
+        SetSeed(seed);
+    }
+
+    /// <summary>
+    /// シード値をセットします
+    /// </summary>
+    /// <param name="seed"></param>
+    public void SetSeed(int seed)
+    {
+        var prevstate = Random.state;
+        Random.InitState(seed);
+        m_state = Random.state;
+        Random.state = prevstate;
+    }
+
+   /// <summary>
+   /// int型のminからmaxまでの範囲内で返します
+   /// </summary>
+   /// <param name="min"></param>
+   /// <param name="max"></param>
+   /// <returns></returns>
+    public int Range(int min, int max)
+    {
+        var prevstate = Random.state;
+        Random.state = m_state;
+        var result = Random.Range(min, max);
+        m_state = Random.state;
+        Random.state = prevstate;
+        return result;
+    }
+
+    /// <summary>
+    /// float型のminからmaxまでの範囲内で返します
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public float Range(float min, float max)
+    {
+        var prevstate = Random.state;
+        Random.state = m_state;
+        var result = Random.Range(min, max);
+        m_state = Random.state;
+        Random.state = prevstate;
+        return result;
+    }
+}
---
+++ Assets/Script/RandomSeedTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSeedTest : MonoBehaviour
+{
+    RandomSeed m_randomSeed = default;
+
+    int m_saiki = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_randomSeed = new RandomSeed(9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void RandomSeed3()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log(i + ":" + m_randomSeed.Range(1, 10));
+        }
+    }
+
+    public void RandomSeedNum(int seed)
+    {
+        m_randomSeed.SetSeed(seed);
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i + ":" + m_randomSeed.Range(1, 10));
+        }
+    }
+
+    public void SaikiTest()
+    {
+        if (m_saiki < 3)
+        {
+            RandomSeed3();
+            m_saiki++;
+            SaikiTest();
+        }
+    }
+}
